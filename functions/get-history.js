9116f8053745ca655e202ed1e0472a1f07d9ec54
ahbabulley <sarongtimbo@gmail.com>
2026-01-16 00:40:35 +0700

    Update get-history.js

--- functions/get-history.js
+++ functions/get-history.js
@@ -1,9 +1,6 @@
 export async function onRequest(context) {
   const history = await context.env.ROWX_DB.get('leads_history');
   return new Response(history || "[]", {
-    headers: { 
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-    }
+    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" }
   });
 }
